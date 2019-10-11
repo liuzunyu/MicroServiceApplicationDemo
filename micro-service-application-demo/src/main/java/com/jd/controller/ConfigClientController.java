43a240ded8ba09ab10534173319383b3762e95b4
liuzunyu <liuzunyu@jd.com>
2019-10-11 11:13:55 +0800

    demo 首次提交

--- micro-service-application-demo/src/main/java/com/jd/controller/ConfigClientController.java
+++ micro-service-application-demo/src/main/java/com/jd/controller/ConfigClientController.java
@@ -9,22 +9,35 @@ import org.springframework.web.bind.annotation.RestController;
  * @author liuzunyu
  * @version 1.0.0
  * @date 2019/9/5
- * @description
+ * @description 版本信息
  */
 @RestController
 // 配置 配置服务中心自动刷新
 @RefreshScope
 public class ConfigClientController {
 
+    /**
+     * 应用版本
+     */
     @Value("${test.version}")
     private String version;
 
+    /**
+     * 应用名称
+     */
     @Value("${spring.application.name}")
     private String name;
 
+    /**
+     * 应用端口
+     */
     @Value("${server.port}")
     private String port;
 
+    /**
+     * 获取应用基本信息
+     * @return 返回版本信息
+     */
     @GetMapping("/getVersion")
     public String version(){
         return "ApplicationName:["+this.name+"] "+" ServerPort:["+this.port+"] Current version: [" + this.version + "]" ;
