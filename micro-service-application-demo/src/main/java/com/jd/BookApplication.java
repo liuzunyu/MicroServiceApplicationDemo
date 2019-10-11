a4fbead74fb5243fe6d521fba6594842f47800a0
liuzunyu <liuzunyu@jd.com>
2019-10-11 11:13:55 +0800

    demo 首次提交

--- micro-service-application-demo/src/main/java/com/jd/BookApplication.java
+++ micro-service-application-demo/src/main/java/com/jd/BookApplication.java
@@ -7,7 +7,17 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+/**
+ * @author liuzunyu
+ * @version 1.0.0
+ * @date 2019/9/5
+ * @description 测试demo
+ */
 public class BookApplication {
+    /**
+     * 启动类
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(BookApplication.class, args);
     }
